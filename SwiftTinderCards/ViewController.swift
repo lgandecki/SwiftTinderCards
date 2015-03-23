bbe465cfc68c6480bd09198d7621bbb9993cb2df
lgandecki <lgandecki@css.edu>
2015-03-23 14:20:32 -0500

    Initial Commit

---
+++ SwiftTinderCards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTinderCards
+//
+//  Created by Lukasz Gandecki on 3/23/15.
+//  Copyright (c) 2015 Lukasz Gandecki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
